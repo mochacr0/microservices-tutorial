984a37d474959e068e939db53ff9d7f832052a9c
NguyenTriHai <nthai2001cr@gmail.com>
2024-01-13 13:27:49 +0700

    config data with consul kv store

--- common-service/src/main/java/com/example/commonservice/controllers/CommonController.java
+++ common-service/src/main/java/com/example/commonservice/controllers/CommonController.java
@@ -1,12 +1,24 @@
 package com.example.commonservice.controllers;
 
+import com.example.commonservice.configs.CommonConfiguration;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CommonController {
+    private final CommonConfiguration configuration;
     @GetMapping("/common/status")
     public String getStatus() {
         return "Common route OK";
+    }
+
+    @GetMapping("/common/config")
+    public String getConfig() {
+        return String.format("Config: %s", configuration.getKey());
     }
 }
